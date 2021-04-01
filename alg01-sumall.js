b55eab1a320a04d12ce380dc814d1a7c8a8dad97
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-01 18:45:45 -0300

    Resoluçao do 1º Algoritmo

--- alg01-sumall.js
+++ alg01-sumall.js
@@ -1,5 +1,67 @@
-function sumAll(arr) {
-  return 1;
+/*
+Source: <https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range>
+
+Sum All Numbers in a Range
+We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+*/
+
+const mySumAll = arr => {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  let sum = 0;
+  for (let index = min; index <= max; index += 1) {
+    sum += index;
+  }
+  return sum;
 }
 
-sumAll([1, 4]);
+const sum1 = mySumAll([4, 1]); //10
+const sum2 = mySumAll([5, 10]); //45
+console.log(sum1, sum2);
+/*
+Get a help > Get a hint <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-sum-all-numbers-in-a-range/16083>
+*/
+//Solution 1
+function sumAll1(arr) {
+  let max = Math.max(arr[0], arr[1]);
+  let min = Math.min(arr[0], arr[1]);
+  let sumBetween = 0;
+  for (let i = min; i <= max; i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
+}
+
+//Solution 2
+const sumAll2 = arr => {
+  // Buckle up everything to one!
+  const startNum = arr[0];
+  const endNum = arr[1];
+
+  // Get the count of numbers between the two numbers by subtracting them and add 1 to the absolute value.
+  // ex. There are |1-4| + 1 = 4, (1, 2, 3, 4), 4 numbers between 1 and 4.
+  const numCount = Math.abs(startNum - endNum) + 1;
+
+  // Using Arithmetic Progression summing formula
+  const sum = ((startNum + endNum) * numCount) / 2;
+  return sum;
+};
+
+//Solution 3
+function sumAll3(arr) {
+  let sumBetween = 0;
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
+}
+
+//Solution 4
+function sumAll4(arr) {
+  const [first, last] = [...arr].sort((a, b) => a - b);
+  return first !== last
+    ? first + sumAll([first + 1, last])
+    : first;
+}
