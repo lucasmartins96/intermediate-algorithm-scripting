1fccafcb072a527ffffd15a8939dcde80aa11a69
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-01 22:00:40 -0300

    Adicionando informaçoes iniciais do 3º algoritmo

---
+++ alg03-destroyer.js
@@ -0,0 +1,16 @@
+/*
+Source: <https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy>
+
+Seek and Destroy
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+Note: You have to use the arguments object.
+*/
+function myDestroyer(arr) {
+  return arr;
+}
+
+myDestroyer([1, 2, 3, 1, 2, 3], 2, 3);
+/*
+Get a help > Get a hint <>
+*/
