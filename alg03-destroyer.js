cdb7865f8ab959a7272ec89391465392e53d9683
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-03 17:40:38 -0300

    Resoluçao do 3º Algoritmo

--- alg03-destroyer.js
+++ alg03-destroyer.js
@@ -7,10 +7,43 @@ You will be provided with an initial array (the first argument in the destroyer 
 Note: You have to use the arguments object.
 */
 function myDestroyer(arr) {
-  return arr;
+  const initialArr = arguments[0];
+  const params = Array.from(arguments).slice(1);
+  return initialArr.filter((element) => !params.includes(element));
 }
 
-myDestroyer([1, 2, 3, 1, 2, 3], 2, 3);
+const out1 = myDestroyer([1, 2, 3, 1, 2, 3], 2, 3); //[1, 1]
+const out2 = myDestroyer([1, 2, 3, 1, 2, 3], 2, 3); //[1, 5, 1]
+const out3 = myDestroyer(["tree", "hamburger", 53], "tree", 53); //["hamburger"]
+const out4 = myDestroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"); //[12, 92, 65]
+console.log(out1, out2, out3, out4);
 /*
-Get a help > Get a hint <>
+Get a help > Get a hint <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-seek-and-destroy/16046>
 */
+//Solution 1
+function destroyer1(arr) {
+  let valsToRemove = Object.values(arguments).slice(1);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(item => item !== null);
+}
+
+//Solution 2
+function destroyer2(arr) {
+  var valsToRemove = Array.from(arguments).slice(1);
+  console.log(valsToRemove);
+  return arr.filter(function(val) {
+    return !valsToRemove.includes(val);
+  });
+}
+
+//Solution 3
+function destroyer3(arr, ...valsToRemove) {
+  return arr.filter(elem => !valsToRemove.includes(elem));
+}
