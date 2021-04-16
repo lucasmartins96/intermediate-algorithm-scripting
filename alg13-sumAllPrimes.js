190da2b0d85e13ff37b9b37fc95eae766fb71777
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-16 11:43:55 -0300

    sumAllPrimes: Adicionando gabarito

--- alg13-sumAllPrimes.js
+++ alg13-sumAllPrimes.js
@@ -33,5 +33,56 @@ assert.strictEqual(typeof mySumPrimes(10), 'number');
 assert.strictEqual(mySumPrimes(10), 17);
 assert.strictEqual(mySumPrimes(977), 73156);
 /*
-Get a help > Get a hint <>
+Get a help > Get a hint <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-sum-all-primes/16085>
 */
+//Solution 1
+function sumPrimes1(num) {
+  // Helper function to check primality
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0)
+        return false;
+    }
+    return true;
+  }
+
+  // Check all numbers for primality
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
+
+//Solution 2
+function sumPrimes2(num) {
+  // Check all numbers for primality
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0))
+      primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+}
+
+//Solution 3
+function sumPrimes3(num) {
+  // Prime number sieve
+  let isPrime = Array(num + 1).fill(true);
+  // 0 and 1 are not prime
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (isPrime[i]) {
+      // i has not been marked false -- it is prime
+      for (let j = i * i; j <= num; j += i)
+        isPrime[j] = false;
+    }
+  }
+
+  // Sum all values still marked prime
+  return isPrime.reduce(
+    (sum, prime, index) => prime ? sum + index : sum, 0
+  );
+}
