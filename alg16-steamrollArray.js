29bb8c3d2292c7b1b49f463fbb69b2be2a2c28bd
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-25 00:03:43 -0300

    steamrollArray: Adicionando gabarito

--- alg16-steamrollArray.js
+++ alg16-steamrollArray.js
@@ -6,10 +6,54 @@ Steamroller
 Flatten a nested array. You must account for varying levels of nesting.
 */
 function mySteamrollArray(arr) {
-  return arr;
+  //Source: <https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/flat>
+  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(mySteamrollArray(val)) : acc.concat(val), []);
+}
+/*
+Get a help > Get a hint <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-steamroller/16079>
+*/
+//Solution 1
+
+//Solution 2
+function steamrollArray2(arr) {
+  const flat = [].concat(...arr);
+  return flat.some(Array.isArray) ? steamrollArray2(flat) : flat;
 }
 
-mySteamrollArray([1, [2], [3, [[4]]]]);
-/*
-Get a help > Get a hint <>
-*/
+//Solution 3
+function steamrollArray3(arr) {
+  return arr
+    .toString()
+    .replace(",,", ",") // "1,2,,3" => "1,2,3"
+    .split(",") // ['1','2','3']
+    .map(function(v) {
+      if (v == "[object Object]") {
+        // bring back empty objects
+        return {};
+      } else if (isNaN(v)) {
+        // if not a number (string)
+        return v;
+      } else {
+        return parseInt(v); // if a number in a string, convert it
+      }
+    });
+}
+
+//Solution 4
+function steamrollArray4(val,flatArr=[]) {
+  val.forEach(item => {
+    if (Array.isArray(item)) steamrollArray4(item, flatArr);
+    else flatArr.push(item);
+  });
+  return flatArr;
+}
+
+//Solution 5
+function steamrollArray5(arr, flatArr = []) {
+  const elem = arr.pop();
+  return elem
+    ? !Array.isArray(elem)
+      ? steamrollArray5(arr, [elem, ...flatArr])
+      : steamrollArray5(arr.concat(elem), flatArr)
+    : flatArr;
+}
