024d892cec66b93034678a44e1072024fabbc437
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-25 10:29:23 -0300

    binaryAgents: Adicionando gabarito

--- alg17-binaryAgents.js
+++ alg17-binaryAgents.js
@@ -26,5 +26,61 @@ assert.strictEqual(
   , 'I love FreeCodeCamp!'
 );
 /*
-Get a help > Get a hint <>
+Get a help > Get a hint <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-binary-agents/14273>
 */
+//Solution 1
+function binaryAgent1(str) {
+  var biString = str.split(" ");
+  var uniString = [];
+
+  /*using the radix (or base) parameter in parseInt, we can convert the binary
+      number to a decimal number while simultaneously converting to a char*/
+
+  for (var i = 0; i < biString.length; i++) {
+    uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+  }
+
+  // we then simply join the string
+  return uniString.join("");
+}
+
+//Solution 2
+function binaryAgent2(str) {
+  // Separate the binary code by space.
+  str = str.split(" ");
+  var power;
+  var decValue = 0;
+  var sentence = "";
+
+  // Check each binary number from the array.
+  for (var s = 0; s < str.length; s++) {
+    // Check each bit from binary number
+    for (var t = 0; t < str[s].length; t++) {
+      // This only takes into consideration the active ones.
+      if (str[s][t] == 1) {
+        // This is quivalent to 2 ** position
+        power = Math.pow(2, +str[s].length - t - 1);
+        decValue += power;
+
+        // Record the decimal value by adding the number to the previous one.
+      }
+    }
+
+    // After the binary number is converted to decimal, convert it to string and store
+    sentence += String.fromCharCode(decValue);
+
+    // Reset decimal value for next binary number.
+    decValue = 0;
+  }
+
+  return sentence;
+}
+
+//Solution 3
+function binaryAgent(str) {
+  return String.fromCharCode(
+    ...str.split(" ").map(function(char) {
+      return parseInt(char, 2);
+    })
+  );
+}
