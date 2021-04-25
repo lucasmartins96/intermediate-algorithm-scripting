952ef08249209a0637ac3bc7260b9bb26f891ce2
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-24 23:19:07 -0300

    steamrollArray: Adicionando o enunciado e o prototipo da funçao.

---
+++ alg16-steamrollArray.js
@@ -0,0 +1,15 @@
+/*
+Source: <https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/steamroller>
+
+Steamroller
+
+Flatten a nested array. You must account for varying levels of nesting.
+*/
+function mySteamrollArray(arr) {
+  return arr;
+}
+
+mySteamrollArray([1, [2], [3, [[4]]]]);
+/*
+Get a help > Get a hint <>
+*/
