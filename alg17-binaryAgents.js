22d5be86bd9e28e91220e49c6bac311f07a70ef2
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-25 10:19:44 -0300

    binaryAgents: Adicionando minha resoluçao

--- alg17-binaryAgents.js
+++ alg17-binaryAgents.js
@@ -9,8 +9,13 @@ Return an English translated sentence of the passed binary string.
 The binary string will be space separated.
 */
 function myBinaryAgent(str) {
-  return str;
+  const translatedSentence = str.split(' ')
+    .map((binary) => String.fromCharCode(parseInt(binary, 2)))
+    .join('');
+ return translatedSentence;
 }
+
+myBinaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111')
 
 assert.strictEqual(
   myBinaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111')
