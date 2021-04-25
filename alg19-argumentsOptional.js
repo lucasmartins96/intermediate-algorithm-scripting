0bb38498c97e72a522812fb65492f233e0cb38f2
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-25 16:02:14 -0300

    argumentsOpcional: Adicionando gabarito

--- alg19-argumentsOptional.js
+++ alg19-argumentsOptional.js
@@ -42,4 +42,70 @@ assert.strictEqual(myAddTogether(2)([3]), undefined);
 /*
 Get a help > Get a hint <>
 */
+//Solution 1
+function addTogether1() {
+  // Function to check if a number is actually a number
+  function checkNum(num) {
+    return (typeof num === "number");
+  };
 
+  if (arguments.length === 2) {
+    // Check if we have two arguments and if they are numbers
+    // Return the sum if they are both numbers
+    let first = arguments[0];
+    let second = arguments[1];
+    if (checkNum(first) && checkNum(second)) {
+      return first + second;
+    } else {
+      return undefined;
+    }
+  } else if (arguments.length === 1) {
+    // If only one argument was found, return a new function
+    let first = arguments[0];
+    if (checkNum(first)) {
+      // Return function that expect a second argument.
+      function addSecond(second) {
+        // Check if the new argument is a number
+        if (checkNum(second)) {
+          return first + second;
+        } else {
+          return undefined;
+        }
+      };
+      return addSecond;
+    } else {
+      return undefined;
+    }
+  } else {
+    // Incorrect number of arguments found
+    return undefined;
+  }
+}
+//Solution 2
+function addTogether2() {
+  const [first, second] = Object.values(arguments);
+  // Check first argument
+  if (typeof first !== "number") {
+    return undefined;
+  }
+
+  // Function to add second argument
+  const addSecond = (second) => typeof second === "number" ? first + second : undefined;
+
+  // Check second argument
+  if (second !== undefined) {
+    return addSecond(second);
+  } else {
+    return addSecond
+  }
+}
+
+//Solution 3
+function addTogether3() {
+  var args = Array.from(arguments);
+  return args.some(n => typeof n !== "number")
+    ? undefined
+    : args.length > 1
+    ? args.reduce((acc, n) => (acc += n), 0)
+    : n => (typeof n === "number" ? n + args[0] : undefined);
+}
