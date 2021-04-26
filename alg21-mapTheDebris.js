59a1ba5ca0469c63c619504c39a16690a7e2ae0c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-04-26 10:54:06 -0300

    mapTheDebris: Adicionando gabarito

--- alg21-mapTheDebris.js
+++ alg21-mapTheDebris.js
@@ -58,9 +58,73 @@ assert.deepStrictEqual(
 Get a help > Get a hint <https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-map-the-debris/16021>
 */
 //Solution 1
+function orbitalPeriod1(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var a = 2 * Math.PI;
+  var newArr = [];
+
+  var getOrbPeriod = function(obj) {
+    var c = Math.pow(earthRadius + obj.avgAlt, 3);
+    var b = Math.sqrt(c / GM);
+    var orbPeriod = Math.round(a * b);
+    // create new object
+    return {name: obj.name, orbitalPeriod: orbPeriod};
+  };
+
+  for (var elem in arr) {
+    newArr.push(getOrbPeriod(arr[elem]));
+  }
+
+  return newArr;
+}
 
 //Solution 2
+function orbitalPeriod2(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var newArr = [];
+
+  //Looping through each key in arr object
+  for (var elem in arr) {
+    //Rounding off the orbital period value
+    var orbitalPer = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(arr[elem].avgAlt + earthRadius, 3) / GM)
+    );
+    //Adding new object with orbitalPeriod property
+    newArr.push({name: arr[elem].name, orbitalPeriod: orbitalPer});
+  }
+
+  return newArr;
+}
 
 //Solution 3
+function orbitalPeriod3(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  // Create new array to prevent modification of the original
+  var newArr = JSON.parse(JSON.stringify(arr));
+  // Loop through each item in the array arr
+  newArr.forEach(function(item) {
+    // Calculate the Orbital period value
+    var tmp = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM)
+    );
+    //Delete the avgAlt property
+    delete item.avgAlt;
+    //Add orbitalPeriod property
+    item.orbitalPeriod = tmp;
+  });
+  return newArr;
+}
 
 //Solution 4
+function orbitalPeriod4(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  return arr.map(({ name, avgAlt }) => {
+    const earth = earthRadius + avgAlt;
+    const orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earth, 3)/GM));
+    return { name, orbitalPeriod };
+  });
+}
